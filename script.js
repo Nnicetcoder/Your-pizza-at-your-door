67472a5a5dae1de681edb30cd9564cbf216e770a
Your Name <nntwari75@gmail.com>
2020-10-11 09:58:46 -0700

    forth commit

--- script.js
+++ script.js
@@ -1,23 +1,63 @@
 $(document).ready(function(){
-   $("#option5").click(function(){
-    $("#option5#yes").click(function(){alert("Your pizza will be delivered in specified location.")};)   	    
-     $("#no").click(function(){alert("Your pizza will not be delivered.")};)     
+   $(".option5").on('change',function(event){
+      var value=$(this).text();
+  if ($(this).find('option:selected').val() === 'YES') {
+      alert('HOLA');
+
+   }
+       	    
+         
    
   }
 
    }
-var small=
+$(".option").click(function(){
 
 
+var small=$("selected#small").val();
+var medium=$("selected#medium").val();
+var large=$("selected#large").val();
+
+})
+
+$(".option").click(function(){
 
 
+var small=$("selected#small").val();
+var medium=$("selected#medium").val();
+var large=$("selected#large").val();
+
+})
 
 
+$(".option2").click(function(){
 
 
+var crispy=$("selected#crispy").val();
+var stuffed=$("selected#stuffed").val();
+var glutenfree=$("selected#gluten free").val();
+
+})
 
 
+$(".option3").click(function(){
 
+
+var beef=$("selected#small").val();
+var chicken=$("selected#medium").val();
+var pork=$("selected#large").val();
+var vegy=$("selected#vegy").val();
+})
+
+
+$(".option").click(function(){
+
+
+var small=$("selected#small").val();
+var medium=$("selected#medium").val();
+var large=$("selected#large").val();
+
+})
 
 
 
