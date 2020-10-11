b69dea4df1d650564b3c2fa2e2e86404b4939ba7
Your Name <nntwari75@gmail.com>
2020-10-11 01:39:48 -0700

    forth commit

---
+++ script.js
@@ -0,0 +1,71 @@
+$(document).ready(function){
+   $(".option5").click(function){
+   	    var YES=$("#input.option5")
+          if (true) {alert("Your pizza will be delivered in specified location.")}
+          else{alert("Your pizza will not be delivered.")}
+
+
+   }
+var small=
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+}
