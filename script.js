eb2fdaeddf3c27baa19e0e214bf2deaa357f9c79
Your Name <nntwari75@gmail.com>
2020-10-11 02:17:37 -0700

    forth commit

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 $(document).ready(function){
-   
+   $(".option5").each(function){
    	    
-          function yesfunction(){alert("Your pizza will be delivered in specified location.")}
-          function nofunction(){alert("Your pizza will not be delivered.")}
+          function yesfunction(){alert("Your pizza will be delivered in specified location.");}
+          function nofunction(){alert("Your pizza will not be delivered.");}
+   }
 
 
    }
